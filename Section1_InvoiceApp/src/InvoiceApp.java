45ec7bf64735bee7b7f7734aee9ce500e0b22efd
lerat <lerat@ImaniLamlaDelli7.ad.wsc.edu>
2023-01-24 23:53:37 -0600

    Initialized all variables and moved them to the start of main.

--- Section1_InvoiceApp/src/InvoiceApp.java
+++ Section1_InvoiceApp/src/InvoiceApp.java
@@ -12,28 +12,36 @@ import java.util.Scanner;
 public class InvoiceApp {
 
     public static void main(String[] args) {
+    	
+    	//initialize variables for use in calculating averages
+    	int invoiceCount = 0;				//stores the number of invoices     
+    	
+    	double discountAmount = 0.0;		//stores how much money gets deducted from the subtotal
+    	double discountPercent = 0.0;		//stores the percentage of the user's discount based off the subtotal
+    	double discountTotal = 0.0;			//stores the total amount of money that is deducted 
+        double invoiceTotal = 0.0;			//stores the total amount of money the user pays in the end with the discounts added
+        double subtotal = 0.0;				//stores the initial amount that the user has to pay before the discount has been added
+        double total = 0.0;					//stores the amount of money the user has to pay after the discount has been applied 
+        
+        //create a Scanner object named sc
+        Scanner sc = new Scanner(System.in);
+        
+        String choice = "y";				//stores the user's choice to either continue running the program or not
+        String input = "";					//will store the amount that the user has to pay in the invoice
+        String message = "";				//will store the summary of the user's invoices
+        
         // welcome the user to the program
         System.out.println("Welcome to the Invoice Total Calculator v2");
         System.out.println();  // print a blank line
 
-        // create a Scanner object named sc
-        Scanner sc = new Scanner(System.in);
-        
-        // initialize variables for use in calculating averages
-        double invoiceTotal = 0.0;
-        double discountTotal = 0.0;
-        int invoiceCount = 0;
-
         // perform invoice calculations until choice is "n" or "N" 
-        String choice = "y";
         while (!choice.equalsIgnoreCase("n")) {
             // get the invoice subtotal from the user
             System.out.print("Enter subtotal:   ");
-            String input = sc.nextLine();
-            double subtotal = Double.parseDouble(input);
+            input = sc.nextLine();
+            subtotal = Double.parseDouble(input);
 
             // calculate the discount amount and total
-            double discountPercent;
             if (subtotal >= 500) {
                 discountPercent = .25;
             } else if (subtotal >= 200) {
@@ -43,8 +51,8 @@ public class InvoiceApp {
             } else {
                 discountPercent = 0.0;
             }
-            double discountAmount = subtotal * discountPercent;
-            double total = subtotal - discountAmount;
+            discountAmount = subtotal * discountPercent;
+            total = subtotal - discountAmount;
             
             // accumulate the invoice count and invoice total
             invoiceTotal = invoiceTotal + total;
@@ -52,7 +60,7 @@ public class InvoiceApp {
             invoiceCount = invoiceCount + 1;  
 
             // display the discount amount and total
-            String message = "Discount percent: " + discountPercent + "\n"
+            message = "Discount percent: " + discountPercent + "\n"
                            + "Discount amount:  " + discountAmount + "\n"
                            + "Invoice total:    " + total + "\n";            
             System.out.println(message);
@@ -67,7 +75,7 @@ public class InvoiceApp {
         sc.close();//closing the scanner
         
         // calculate and display invoice count, average invoice, and average discount
-        String message = "Number of invoices: " + invoiceCount + "\n"
+        message = "Number of invoices: " + invoiceCount + "\n"
                        + "Average invoice:    " + invoiceTotal / invoiceCount + "\n"
                        + "Average discount:   " + discountTotal / invoiceCount + "\n";
         System.out.println(message);
